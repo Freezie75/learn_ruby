e5ed97e1bdb49d01389fd13058fbe5a9833a6ba2
Freezie75 <nathanbittoun@yahoo.fr>
2018-04-09 22:50:36 +0200

    Exercice 6 terminé

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,40 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  # On défini les secondes = 0 de base
+  def initialize
+    @seconds = 0
+    return @seconds
+  end
+
+  def time_string
+    # Une heure = 3600s
+    # Une minute = 60s
+    # On calcule les temps en prenant en compte cette base la
+    @heures = @seconds / 3600.to_i
+    @minutes = (@seconds / 60 - @heures * 60).to_i
+    @seconds = (@seconds - @minutes * 60 - @heures * 3600).to_i
+
+    # Boucle d'affichage en fonction du nombre de secondes
+    if @minutes > 10
+      @minutes = "#{minutes}"
+    else
+      @minutes = "0#{@minutes}"
+    end
+
+    if @seconds > 10
+      @seconds = "#{@seconds}"
+    else
+      @seconds = "0#{@seconds}"
+    end
+
+    if @heures > 10
+      @heures = "#{@heures}"
+    else
+      @heures = "0#{@heures}"
+    end
+  end
+
+  # Résultat finale
+  return "" + @heures + ":" + @minutes + ":" + @seconds
 end
