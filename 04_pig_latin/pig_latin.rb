432cbc3f2d7e5ca9e056f1d9b731d8ced3ec73c6
Freezie75 <nathanbittoun@yahoo.fr>
2018-04-09 21:45:48 +0200

    Correction de l'exercice 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -20,35 +20,35 @@ Plusieurs cas :
 on rajoute juste un array à la fin
 2 - La 1ere lettre est un Q suivi de U ou
 alors on considere SQU comme une consonne
-3 - 3 consonnes
-4 - 2 consonnes
-5 - 1 consonne
+3 - Trois consonnes
+4 - Deux consonnes
+5 - Une consonne
 =end
-  @whole_sentence.each do |word_to_translate|
+@whole_sentence.each do |word_to_translate|
     
-    @first_char = word_to_translate[0]
-    @second_char = word_to_translate[1]
-    @third_char = word_to_translate[2]
-    @text_length = word_to_translate.length
-    @new_word = word_to_translate
-    @nq = @first_char + @second_char + @third_char
+  @first_char = word_to_translate[0]
+  @second_char = word_to_translate[1]
+  @third_char = word_to_translate[2]
+  @text_length = word_to_translate.length
+  @new_word = word_to_translate
+  @nq = @first_char + @second_char + @third_char
 
-    if @vowels.include?(@first_char)
-       # Ici on commence par une voyelle
+  if @vowels.include?(@first_char)
+      # Ici on commence par une voyelle
          
-    elsif @qu.include?("qu")
-      @new_word[@text_length] = @first_char
-      @new_word[@text_length + 1] = @second_char
+     elsif @qu.include?("qu")
+       @new_word[@text_length] = @first_char
+       @new_word[@text_length + 1] = @second_char
       
-      @index = @new_word.index("q")
-      if @index >= 1
-        @new_word[text_length + 2] = @third_char
-        @new_word = @new_word[@index + 2..-1]
-      else 
-        @new_word = @new_word[@index + 2..-1]
-      end
+       @index = @new_word.index("q")
+       if @index >= 1
+         @new_word[text_length + 2] = @third_char
+         @new_word = @new_word[@index + 2..-1]
+       else 
+         @new_word = @new_word[@index + 2..-1]
+        end
 
-    elsif !(@vowels.include?(@first_char) || @vowels.include?(@second_char) || @vowels.include?(@third_char))
+      elsif !(@vowels.include?(@first_char) || @vowels.include?(@second_char) || @vowels.include?(@third_char))
 
 # Ici on commence par trois consonnes
       @new_word[@text_length] = @first_char
@@ -65,7 +65,7 @@ alors on considere SQU comme une consonne
     else
       @new_word[@text_length] = @first_char
       @new_word = @new_word[1..-1]
-      # Ici on consonne par une consonne
+      # Ici on commence par une consonne
     end
       @new_word += "ay"
       @output += @new_word + " "
