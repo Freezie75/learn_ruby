d56aa4ee9f2b9af36001b95158bf9cc8fff0b660
Freezie75 <nathanbittoun@yahoo.fr>
2018-04-09 22:18:49 +0200

    Exercice 5 terminé

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,46 @@
 class Book
-# write your code here
+  def title # On initialise avec cette méthode
+    @title
+  end
+
+  # Cette méthode appelle la fonction et retourne le tout
+  def title=(string)
+    @title = capital_letters(string)
+    return @title
+  end
+
+  def capital_letters(title)
+    # Déclaration des variables
+    @title = title
+
+    # On met tout les mots dans un array
+    @array_title = @title.split()
+    @capitalized_movies = []
+    @return = ""
+    i = 0
+
+    # On boucle sur les mots pour les mettre en capitale
+    # sauf si ce sont des And in the of a an (exception
+    # de The en premiere position)
+    @array_title.each do |x|
+
+      # Boucle i'f qui contient les exceptions
+      stop_words = %w(and in the of a an)
+      if stop_words.include?(x) && i != 0
+        @capitalized_movies << x
+      elsif
+        x = x.capitalize
+        @capitalized_movies << x
+      end
+      i = i + 1
+    end
+
+  @capitalized_movies.each do |x|
+    @return += x + " "
+  end
+
+# On coupe le dernier espace et c'est bon
+@return = @return.chomp(' ')
+return @return
+end
 end
